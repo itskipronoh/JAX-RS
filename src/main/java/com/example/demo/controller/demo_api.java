d48b878d46d7af646a7a0556d9979303960a62e1
itskipronoh <kipronoyegon68@gmail.com>
2025-03-24 23:41:47 +0300

    postman actions

--- src/main/java/com/example/demo/controller/demo_api.java
+++ src/main/java/com/example/demo/controller/demo_api.java
@@ -2,20 +2,25 @@ package com.example.demo.controller;
 
 
 import com.example.demo.model.demo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/demo")
 
 public class demo_api
 {
+
     @GetMapping("/{demoId}")
     public demo getDemoDetails(String demoId) {
         // Create and return a demo object with the provided ID
         return new demo(demoId, "Sample Name", "Sample Address", "123-4567");
     }
 
+    @PostMapping("/")
+    public String createDemoDetails(@RequestBody demo demo)
+    {
+        return ("demo created successfully");
+
+    }
 
 }
